4188ae7a0d34f0c9512751a4c478fac5a146eb14
Marianna Weychan <marianna.weychan@wp.pl>
2022-11-23 15:22:17 +0100

    remove task interaction

--- src/Tasks/index.js
+++ src/Tasks/index.js
@@ -1,6 +1,6 @@
 import "./style.css";
 
-const Tasks = ({ tasks, hideDone }) => (
+const Tasks = ({ tasks, hideDone, removeTask }) => (
     <ul className="tasks">
         {tasks.map(task => (
             <li key={task.id}
@@ -16,7 +16,10 @@ const Tasks = ({ tasks, hideDone }) => (
                 }>
                     {task.content}
                 </span>
-                <button className="tasks__button tasks__button--delete">
+                <button
+                    className="tasks__button tasks__button--delete"
+                    onClick={() => removeTask(task.id)}
+                >
                     🗑
                 </button>
             </li>
