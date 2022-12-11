b82f8522d6a8df5e5b8de39458d9ec9da1cb541c
Marianna Weychan <marianna.weychan@wp.pl>
2022-12-11 13:25:53 +0100

    theme added

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { ThemeProvider } from "styled-components"
-import { theme } from "./theme"
+import { ThemeProvider } from "styled-components";
+import { theme } from "./theme";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
---
+++ src/theme.js
@@ -0,0 +1,19 @@
+export const theme = {
+    colors: {
+        teal: "#008080",
+        white: "#FFFFFF",
+        bondiBlue: "#00B3B3",
+        java: "#20B6B6",
+        viking: "#71CDDA",
+        parsley: "#18671F",
+        green: "#00B300",
+        mintGreen: "#92F792",
+        milanoRed: "#C60C0C",
+        mandy: "#E15656",
+        mandysPink: "#F4C7C7",
+    },
+
+    breakpoint: {
+        mobile: 767
+    },
+};
