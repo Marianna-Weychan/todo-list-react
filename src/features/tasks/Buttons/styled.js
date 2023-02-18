8a2e194fc4f381e9f50b8514ee4c8a27779c28d8
Marianna Weychan <marianna.weychan@wp.pl>
2023-02-18 11:39:41 +0100

    added line indent and poniter-events: none for disabled button

--- src/features/tasks/Buttons/styled.js
+++ src/features/tasks/Buttons/styled.js
@@ -12,21 +12,23 @@ export const StyledButton = styled.button`
     border: none;
     margin: 0 0 0 20px;
     transition: 0.3s;
+    cursor: pointer;
 
 @media(max-width: ${({ theme }) => theme.breakpoint.mobile}px) {
     flex-basis: 100%;
     margin: 10px;
     }
    
-&:hover {
-   filter: brightness(130%);
-}
+        &:hover {
+        filter: brightness(130%);
+        }
 
-&:active {
-    filter: brightness(160%);
-}
+        &:active {
+            filter: brightness(160%);
+        }
 
-&:disabled {
-   color: ${({ theme }) => theme.colors.white};
-}
+        &:disabled {
+        color: ${({ theme }) => theme.colors.iron};
+        pointer-events: none
+        }
 `;
